21e9223e48ca222f9fb49ee4f3c49f515f964795
leticia-nascimento-11a <leticia.conce@eaportal.org>
2025-04-29 05:41:56 -0300

    Segundo commit e exemplo

--- main.js
+++ main.js
@@ -8,3 +8,19 @@
    
    mensagem()
  */
+ 
+ 
+ 
+ // exemplo 2
+ 
+ /*
+ function somar(numero1, numero2) {
+     console.log(numero1 + numero2)
+ }
+   
+ let numero1 = 20;
+ let numero2 = 30;
+ somar(numero1, numero2)
+ somar(30,12)
+ somar(19,289)
+ */
